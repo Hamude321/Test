07ec51a0f77b2355cb4f835c8acfb016d994c95b
mmahmoud <mmahmoud@AC-LPDJA2.ametras.com>
2023-03-14 09:04:02 +0100

    Branch test

---
+++ kontrollsatz_rcp/src/alt/test.java
@@ -0,0 +1,14 @@
+//package alt;
+//
+//import java.io.IOException;
+//
+//public class test {
+//
+//	public static void main(String[] args) throws IOException {
+//		// TODO Auto-generated method stub
+//		JavaReader test = new JavaReader();
+//		test.read();
+//
+//	}
+//
+//}
